e8df348357adad0def2c22f3c3b3abb89653a233
japzio <jculongit10@gmail.com>
2022-08-22 09:25:15 +0800

    test containers yt exercise

--- src/test/java/com/jetbrains/testcontainersdemo/CustomerIntegrationTests.java
+++ src/test/java/com/jetbrains/testcontainersdemo/CustomerIntegrationTests.java
@@ -3,16 +3,35 @@ package com.jetbrains.testcontainersdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Testcontainers
 public class CustomerIntegrationTests {
 
     @Autowired
     private CustomerDao customerDao;
+
+
+    @Container
+    private static MySQLContainer container = new MySQLContainer("mysql:8.0")
+            .withDatabaseName("somedatabase")
+            .withUsername("root")
+            .withPassword("letsgomarco");
+
+    @DynamicPropertySource
+    public static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+
+    }
 
     @Test
     void when_using_a_clean_db_this_should_be_empty() {
